9a68634cbc1227a483e817d9e105c94fa0d46953
Brian Kessler <brian.m.kessler@gmail.com>
2016-10-30 22:07:29 -0700

    Completed writing of WAV files *  Handle 8-bit offset encoding correctly *  Multiple channels, i.e. stereo *  Added some basic tests to writing tones

--- afsk1200.go
+++ afsk1200.go
@@ -1,7 +1,10 @@
 package aprsgo
 
 import (
+	"encoding/binary"
+	"errors"
 	"math"
+	"os"
 )
 
 type symbolWriter interface {
@@ -14,6 +17,13 @@ const (
 	spaceFreq  float64 = 2200.0 // mark frequency in Hertz
 )
 
+var (
+	riffTag = "RIFF" // RIFF tag header for entire file
+	waveTag = "WAVE" // WAVE tag header identifying type of RIFF
+	fmtTag  = "fmt " // fmt tag header for format chunk
+	dataTag = "data" // data tag header for data chunk
+)
+
 var scalingFactor = 0.75 // the scaling factor for volume between 0.0 and 1.0
 
 var symbolFrequency = map[symbol]float64{mark: markFreq, space: spaceFreq}
@@ -21,6 +31,7 @@ var symbolFrequency = map[symbol]float64{mark: markFreq, space: spaceFreq}
 type waveWriter struct {
 	samplesPerSecond     uint32             // ideally a multiple of 1200, e.g. 48000 DVD sound
 	bitsPerSample        uint8              // supported values are 8, 12, 32
+	numChannels          uint8              // 1 mono, 2 stereo
 	volumeLevel          float64            // the scaling factor for the samples to full scale
 	symbolCount          uint32             // the count of the current number of symbols in this second
 	samplesPerSymbol     uint32             // how many samples are written for each symbol
@@ -30,10 +41,30 @@ type waveWriter struct {
 	data                 []byte             // the output data as an array of bytes
 }
 
-func newWaveWriter(samplesPerSecond uint32, bitsPerSample uint8) *waveWriter {
+type waveHeader struct {
+	riffChunkID           [4]byte // "RIFF"
+	riffChunkSize         uint32  // 4 + (8 + formatChunkSize) + (8 + dataChunkSize) = 36 + dataChunkSize
+	waveChunkID           [4]byte // "WAVE"
+	formatChunkID         [4]byte // "fmt "
+	formatChunkSize       uint32  // 16 for PCM
+	waveFormatTag         uint16  // 0x0001 for PCM
+	numberOfChannels      uint16  // Nc
+	samplesPerSecond      uint32  // sampling frequency, e.g. 48000
+	averageBytesPerSecond uint32  // F*M*Nc
+	blockAlign            uint16  // M*Nc
+	bitsPerSample         uint16  // 8*M
+	dataChunkID           [4]byte // "data"
+	dataChunkSize         uint32  // M*Nc*Ns
+}
+
+func NewWaveWriter(samplesPerSecond uint32, bitsPerSample uint8, numChannels uint8) (*waveWriter, error) {
+	if bitsPerSample%8 != 0 || bitsPerSample > 32 {
+		return nil, errors.New("only 8, 16, 24 and 32 bitsPerSample are supported")
+	}
 	writer := new(waveWriter)
 	writer.samplesPerSecond = samplesPerSecond
 	writer.bitsPerSample = bitsPerSample
+	writer.numChannels = numChannels
 	writer.volumeLevel = scalingFactor * float64(uint64(1)<<(bitsPerSample-1)) // the level
 	writer.samplesPerSymbol = samplesPerSecond / symbolRate
 	writer.skewSamples = samplesPerSecond % symbolRate // the remainder will need to be skewed in to prevent drift
@@ -41,7 +72,7 @@ func newWaveWriter(samplesPerSecond uint32, bitsPerSample uint8) *waveWriter {
 	phaseIncrementSymbol[mark] = 2 * math.Pi * markFreq / float64(samplesPerSecond)
 	phaseIncrementSymbol[space] = 2 * math.Pi * spaceFreq / float64(samplesPerSecond)
 	writer.phaseIncrementSymbol = phaseIncrementSymbol
-	return writer
+	return writer, nil
 }
 
 func (w *waveWriter) WriteSymbol(sym symbol) error {
@@ -63,20 +94,56 @@ func (w *waveWriter) writeSample(phaseIncrement float64) {
 		w.currentPhase -= 2 * math.Pi
 	}
 	newSample := w.volumeLevel * math.Sin(w.currentPhase)
-	switch { // handle different bit rates properly
-	case w.bitsPerSample == 8: // 8-bit just write the bytes
-		w.data = append(w.data, byte(newSample))
-	case w.bitsPerSample == 16: // 16-bit write LSB then MSB
-		u16Sample := uint16(newSample)
-		var h, l byte = byte((u16Sample >> 8) & 0xff), byte(u16Sample & 0xff)
-		w.data = append(w.data, l)
-		w.data = append(w.data, h)
-	case w.bitsPerSample == 32: // 32-bit write from LSB to MSB
-		u32Sample := uint32(newSample)
-		var hh, hl, lh, ll byte = byte((u32Sample >> 24) & 0xff), byte((u32Sample >> 16) & 0xff), byte((u32Sample >> 8) & 0xff), byte(u32Sample & 0xff)
-		w.data = append(w.data, ll)
-		w.data = append(w.data, lh)
-		w.data = append(w.data, hl)
-		w.data = append(w.data, hh)
+	for i := uint8(0); i < w.numChannels; i++ { // write one sample for each channel
+		u32Sample := uint32(newSample) // bits per sample only supported multiples of 8 up to 32
+		if w.bitsPerSample == 8 {
+			u32Sample += (1 << 7) // 8-bit is offset encoded
+			u32Sample %= (1 << 8)
+		}
+		for i := uint8(0); i < w.bitsPerSample/8; i++ {
+			w.data = append(w.data, byte(u32Sample&0xFF))
+			u32Sample = u32Sample >> 8
+		}
 	}
+}
+
+func (w *waveWriter) WriteFile(filename string) error {
+	data := w.data
+	if len(w.data)%2 != 0 {
+		data = append(data, byte(0)) // pad a zero byte if the length is not even
+	}
+	M := w.bitsPerSample / 8 // Bytes per sample
+	Nc := w.numChannels      // number of channels
+	Ns := len(data)          // the total number of samples, with padding
+	header := waveHeader{
+		riffChunkSize:         uint32(4 + 26 + 12 + (8 + uint32(M)*uint32(Nc)*uint32(Ns))),
+		formatChunkSize:       uint32(16),
+		waveFormatTag:         uint16(0x0001),
+		numberOfChannels:      uint16(Nc),
+		samplesPerSecond:      uint32(w.samplesPerSecond),
+		averageBytesPerSecond: w.samplesPerSecond * uint32(M) * uint32(Nc),
+		blockAlign:            uint16(M) * uint16(Nc),
+		bitsPerSample:         uint16(w.bitsPerSample),
+		dataChunkSize:         uint32(M) * uint32(Nc) * uint32(Ns),
+	}
+	copy(header.riffChunkID[:], riffTag)
+	copy(header.waveChunkID[:], waveTag)
+	copy(header.formatChunkID[:], fmtTag)
+	copy(header.dataChunkID[:], dataTag)
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	err = binary.Write(file, binary.LittleEndian, header)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(file, binary.LittleEndian, w.data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- afsk1200_test.go
+++ afsk1200_test.go
@@ -1,11 +1,15 @@
 package aprsgo
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestNewWaveWriter(t *testing.T) {
-	wr := newWaveWriter(48000, 8)
+	wr, err := NewWaveWriter(48000, 8, 1)
+	if err != nil {
+		t.Errorf("Errors creating 48kHz sampling at 8-bits per second: %v", err)
+	}
 	if wr.samplesPerSymbol != 40 {
 		t.Errorf("48kHz sampling should have 40 samples per symbol, has %v", wr.samplesPerSymbol)
 	}
@@ -15,7 +19,7 @@ func TestNewWaveWriter(t *testing.T) {
 	if wr.volumeLevel != 96 {
 		t.Errorf("8-bit volume level with scaling 0.75 should be XXX, got %v", int8(wr.volumeLevel))
 	}
-	wr = newWaveWriter(44100, 16)
+	wr, err = NewWaveWriter(44100, 16, 1)
 	if wr.samplesPerSymbol != 36 {
 		t.Errorf("44.1kHz sampling should have 36 samples per symbol, has %v", wr.samplesPerSymbol)
 	}
@@ -24,5 +28,63 @@ func TestNewWaveWriter(t *testing.T) {
 	}
 	if wr.volumeLevel != 24576 {
 		t.Errorf("16-bit volume level with scaling 0.75 should be 24576, got %v", int16(wr.volumeLevel))
+	}
+	wr, err = NewWaveWriter(44100, 12, 1)
+	if err == nil {
+		t.Errorf("Created an unsupported wave writer with 12-bits per sample")
+	}
+	wr, err = NewWaveWriter(44100, 64, 1)
+	if err == nil {
+		t.Errorf("Created an unsupported wave writer with 64-bits per sample")
+	}
+}
+
+func TestWriteSymbol(t *testing.T) {
+	sampleRate, bitsPerSample, channels := uint32(48000), uint8(8), uint8(1)
+	wr, err := NewWaveWriter(sampleRate, bitsPerSample, channels)
+	if err != nil {
+		t.Errorf("Errors creating %vkHz sampling at %v-bits per second: %v", sampleRate/1000, bitsPerSample, err)
+	}
+	for i := uint32(0); i < symbolRate; i++ { // write one second of marks
+		wr.WriteSymbol(mark)
+	}
+	if len(wr.data) != int(sampleRate)*int(channels*bitsPerSample)/8 {
+		t.Errorf("%v samples, should be %v", len(wr.data), int(sampleRate)*int(bitsPerSample)/8)
+	}
+	err = wr.WriteFile("test_48000Hz_8bit_1200_mark.wav")
+	if err != nil {
+		t.Errorf("Error saving file: %v", err)
+	}
+
+	sampleRate, bitsPerSample = uint32(44100), uint8(16)
+	wr, err = NewWaveWriter(sampleRate, bitsPerSample, channels)
+	if err != nil {
+		t.Errorf("Errors creating %vkHz sampling at %v-bits per second: %v", sampleRate/1000, bitsPerSample, err)
+	}
+	for i := uint32(0); i < symbolRate; i++ { // write one second of spaces
+		wr.WriteSymbol(space)
+	}
+	if len(wr.data) != int(sampleRate)*int(channels*bitsPerSample)/8 {
+		t.Errorf("%v samples, should be %v", len(wr.data), int(sampleRate)*int(bitsPerSample)/8)
+	}
+	err = wr.WriteFile("test_44100Hz_16bit_1200_space.wav")
+	if err != nil {
+		t.Errorf("Error saving file: %v", err)
+	}
+
+	sampleRate, bitsPerSample, channels = uint32(48000), uint8(16), 2
+	wr, err = NewWaveWriter(sampleRate, bitsPerSample, channels)
+	if err != nil {
+		t.Errorf("Errors creating %vkHz sampling at %v-bits per second: %v", sampleRate/1000, bitsPerSample, err)
+	}
+	for i := uint32(0); i < symbolRate; i++ { // write one second of marks in stereo
+		wr.WriteSymbol(mark)
+	}
+	if len(wr.data) != int(sampleRate)*int(channels*bitsPerSample)/8 {
+		t.Errorf("%v samples, should be %v", len(wr.data), int(sampleRate)*int(bitsPerSample)/8)
+	}
+	err = wr.WriteFile(fmt.Sprintf("test_%vHz_%vbit_1200_mark_%vchan.wav", sampleRate, bitsPerSample, channels))
+	if err != nil {
+		t.Errorf("Error saving file: %v", err)
 	}
 }
